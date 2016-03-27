e3aadefb75f8340ae0947c6070606347c7628987
theory friction practice <jackson.brebnor@gmail.com>
2016-03-27 12:42:41 -0400

    YARD documentation for IPrimate and prepend script improvements

--- bin/tfp_metaprogramming/object_model/prepend.rb
+++ bin/tfp_metaprogramming/object_model/prepend.rb
@@ -9,33 +9,40 @@ This new method #primate returns information about the Module,
 and the #reverse method overrides Ruby's String#reverse"
 puts
 puts "Now creating a new namespaced string
-myString = TfpMetaprogramming::ObjectModel::String.new(hello)"
-myString = TfpMetaprogramming::ObjectModel::String.new("hello")
-puts "Now calling myString.primate
-which should output 'I am a the included primate (IPrimate) module and I override String#reverse'"
-puts "..."
-puts myString.primate
+my_string = TfpMetaprogramming::ObjectModel::String.new(hello)"
+my_string = TfpMetaprogramming::ObjectModel::String.new("hello")
+puts "Now showing the ancestor chain of my_string
+via my_string.class.ancestors"
+puts my_string.class.ancestors
+puts "Now calling my_string.primate
+which should output
+'I am a the included primate (IPrimate) module and I override String#reverse'
+..."
+puts my_string.primate
 puts
 puts "Using ruby 2's prepend functionality,
 this class also has a method #prep_primate, which prepends
-the module TfpMetaprogramming::ObjectModel::Primate (as opposed to the former IPrimate).
-THis module which also includes the reverse method.
-due to this prepending, of the Primate module BELOW the class itself in the ancestor chain
-when reverse is called, IPrimate#reverse is overwritten by Primate#reverse, and is never called"
-puts "Now calling myString.reverse (before calling prep_primate),
+the module TfpMetaprogramming::ObjectModel::Primate
+(as opposed to the former IPrimate).
+This module which also includes the reverse method.
+due to this prepending, of the Primate module BELOW the class itself
+in the ancestor chain. when reverse is called, IPrimate#reverse
+is overwritten by Primate#reverse, and is never called
+
+Now calling my_string.reverse (before calling prep_primate),
 which should output 
 'overwrites String#reverse: fruit====tiurf'
 ..."
-puts myString.reverse
-puts "Now calling myString.prep_primate"
-myString.prep_primate
-# myString.reverse
+puts my_string.reverse
+puts "Now calling my_string.prep_primate"
+my_string.prep_primate
+# my_string.reverse
 puts "as you can see, in the ancestors chain for the namespaced String class
 the Primate module comes before the IPrimate is as follows"
-puts "now calling myString.class.ancestors"
+puts "now calling my_string.class.ancestors"
 puts "..."
-puts myString.class.ancestors
-puts "Now calling myString.reverse (before calling prep_primate),
+puts my_string.class.ancestors
+puts "Now calling my_string.reverse (before calling prep_primate),
 which should output 
 'I am the prepended Primate module and I output banana====ananab'
 ..."
--- lib/tfp_metaprogramming/object_model/prepend/i_primate.rb
+++ lib/tfp_metaprogramming/object_model/prepend/i_primate.rb
@@ -1,9 +1,15 @@
-# This Module includes #reverse method, overriding Ruby's String#reverse
+# This Module will be included in TfpMetaprogramming::ObjectModel::String
+# to overwrite Object::String#reverse
 module TfpMetaprogramming::ObjectModel::IPrimate
+  # overwrites Object::String#reverse when included
+  #
+  # @return [String] "overwrites String#reverse: fruit====tiurf"
   def reverse
     "overwrites String#reverse: fruit====tiurf"
   end
-
+  # returns information about the Module
+  #
+  # @return [String] "I am a the included primate (IPrimate) module and I override String#reverse"
   def primate
     "I am a the included primate (IPrimate) module and I override String#reverse"
   end
