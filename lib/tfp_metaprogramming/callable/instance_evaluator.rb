d04c11186bc5bd0bea2599cab82141f2ea676eb9
theory friction practice <jackson.brebnor@gmail.com>
2016-03-31 11:22:47 -0400

    adds/test instance eval/exec to share instance varaibles between objects within a block

--- lib/tfp_metaprogramming/callable/instance_evaluator.rb
+++ lib/tfp_metaprogramming/callable/instance_evaluator.rb
@@ -1,13 +1,52 @@
 require_relative 'bindr'
-
 module TfpMetaprogramming
   module Callable
     class InstanceEvaluator
+      attr_reader :bindr, :ivar
+
       def initialize(bindr = TfpMetaprogramming::Callable::Bindr.new )
         @bindr = bindr
+        @ivar = 3
       end
 
+      def bindr_self
+        bindr.instance_eval { self }
+      end
 
+      def bindr_locals
+        bindr.instance_eval { local_variables }
+      end
+
+      def bindr_class_locals
+        bindr.class.instance_eval { local_variables }
+      end
+
+      def modify_bindr_info
+        modText = 'called from evaluator'
+        bindr.instance_eval { @info += modText }
+      end
+
+      def show_bindr_num_and_evaluator_ivar
+        bindr.instance_eval do
+          result = "bindr num = #{num}"
+          # result +=
+          if
+            defined?(ivar)
+            result += " evaluator ivar #{ivar}"
+          else
+            result += " within this scope, evaluator ivar is undefined"
+          end
+          result
+        end
+      end
+
+      def execute_sum_bindr_num_and_evaluator_ivar
+        bindr.instance_exec(ivar) do |ivar|
+          result = "#{num+ivar} is the sum,
+      		because bindr num is #{num},
+      		and evaluator ivar is #{ivar}"
+        end
+      end
     end
   end
 end
--- spec/callable/instance_evaluator_spec.rb
+++ spec/callable/instance_evaluator_spec.rb
@@ -4,4 +4,37 @@ describe TfpMetaprogramming::Callable::InstanceEvaluator do
   it 'has a @bindr object' do
     expect(myEvaluator.instance_variables).to include(:@bindr)
   end
+  describe 'methods' do
+    describe '#bindr_self' do
+      it 'calls #self on the bindr object' do
+        expect(myEvaluator.bindr_self).to eq(eBindr)
+      end
+      describe '#bindr_locals' do
+        it 'returns local_variables on the @bindr' do
+          expect(myEvaluator.bindr_locals).to be_a_kind_of(Array)
+        end
+      end
+      describe '#bindr_class_locals' do
+        it 'evaluates local_variables on the @bindr class' do
+          expect(myEvaluator.bindr_class_locals).to be_a_kind_of(Array)
+        end
+      end
+      describe '#modify_bindr_info' do
+        it 'appends modText="called from the evaluator" to the bindr.info' do
+          expect { myEvaluator.modify_bindr_info }.to change { eBindr.info }
+        end
+      end
+      describe '#show_bindr_num_and_evaluator_ivar' do
+        it 'displays @bindr.num and #ivar' do
+          expect(myEvaluator.show_bindr_num_and_evaluator_ivar).to end_with('undefined')
+        end
+      end
+      describe '#execute_sum_bindr_num_and_evaluator_ivar' do
+        it 'begins with the sum of both InstanceEvaluator#ivar and @bindr.num' do
+          sum = myEvaluator.ivar + myEvaluator.bindr.num
+          expect(myEvaluator.execute_sum_bindr_num_and_evaluator_ivar).to start_with("#{sum}")
+        end
+      end
+    end
+  end
 end
