210202183fb3b9aa5bce463a09e0b9f43cb5425a
theory friction practice <jackson.brebnor@gmail.com>
2016-04-01 10:05:26 -0400

    Add/test Procster#sum_with_proc

--- spec/callable/procs/procster_spec.rb
+++ spec/callable/procs/procster_spec.rb
@@ -4,5 +4,13 @@ describe TfpMetaprogramming::Callable::Procs::Procster do
     it 'has a @num' do
       expect(myProcster.instance_variables).to include(:@num)
     end
+    it 'has an @info' do
+      expect(myProcster.instance_variables).to include(:@num)
+    end
+  end
+  describe 'ProcMod module' do
+    it 'includes Callable::Procs::ProcMod in its ancestor chain' do
+      expect(myProcster.class.ancestors).to include(TfpMetaprogramming::Callable::Procs::ProcMod)
+    end
   end
 end
