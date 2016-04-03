a74027635ef4f1059f614819ae4040674b0e6ad6
theory friction practice <jackson.brebnor@gmail.com>
2016-04-02 21:17:09 -0400

    tests nested def technique to add singleton #mingle to string

--- lib/tfp_metaprogramming/meta_class/singletons/singlizer.rb
+++ lib/tfp_metaprogramming/meta_class/singletons/singlizer.rb
@@ -2,11 +2,18 @@ module TfpMetaprogramming
   module MetaClass
     module Singletons
       module Singlizer
-        def self.add_monog_to_single_string(iString)
+        def self.eval_monog_onto_single_string(iString)
           iString.instance_eval do
             def self.monogamize
-              self <<' I am monogamous'
+              self << ' I am monogamous'
             end
+          end
+        end
+
+        def self.nest_mingle_method_onto_single_string(iString)
+          def iString.mingle
+            self << ' and I am ready to mingle'
+
           end
         end
       end
--- spec/meta_class/singletons/singlizer_spec.rb
+++ spec/meta_class/singletons/singlizer_spec.rb
@@ -2,8 +2,9 @@ describe TfpMetaprogramming::MetaClass::Singletons::Singlizer do
   let(:myMod) { TfpMetaprogramming::MetaClass::Singletons::Singlizer }
   let(:sample) { TfpMetaprogramming::MetaClass::Singletons::SingleString.new('A new single string') }
   let(:pristine_sample) { TfpMetaprogramming::MetaClass::Singletons::SingleString.new('A pristine single string') }
+  let(:minglingString) { TfpMetaprogramming::MetaClass::Singletons::SingleString.new('A new single and mingling string') }
 
-  describe '.add_monog_to_single_string' do
+  describe '.eval_monog_onto_single_string' do
     context 'when given a string (sample) to work with' do
       context 'before called' do
         it 'excludes #monogamize from that string' do
@@ -12,9 +13,9 @@ describe TfpMetaprogramming::MetaClass::Singletons::Singlizer do
       end
       context 'after_called' do
         before(:each) do
-          myMod.add_monog_to_single_string(sample)
+          myMod.eval_monog_onto_single_string(sample)
         end
-        it 'adds singleton method #monogamize to that particular string' do
+        it 'instance_eval adds singleton method #monogamize particular string' do
           expect(sample.methods).to include(:monogamize)
         end
         it 'does not add #monogamize to any other' do
@@ -28,5 +29,30 @@ describe TfpMetaprogramming::MetaClass::Singletons::Singlizer do
       end
     end
 
+  end
+  describe '.nest_mingle_method_onto_single_string' do
+    context 'when given a string (minglingString) to work with' do
+      context 'before called' do
+        it 'excludes #mingle from that string' do
+          expect(minglingString.methods).not_to include(:mingle)
+        end
+      end
+      context 'after_called' do
+        before(:each) do
+          myMod.nest_mingle_method_onto_single_string(minglingString)
+        end
+        it 'nested def adds singleton method #mingle particular string' do
+          expect(minglingString.methods).to include(:mingle)
+        end
+        it 'does not add #mingle to any other' do
+          expect(pristine_sample.methods).not_to include(:mingle)
+        end
+        describe '#mingle' do
+          it 'concatenates " and I am ready to mingle" to the given string' do
+            expect(minglingString.mingle).to end_with("mingle")
+          end
+        end
+      end
+    end
   end
 end
