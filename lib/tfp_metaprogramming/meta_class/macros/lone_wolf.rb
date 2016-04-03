07880e939f024da3a0c9b091daf0181aba1448c0
theory friction practice <jackson.brebnor@gmail.com>
2016-04-03 19:10:41 -0400

    tests LoneWolf.contemplate_solipsism and .resolve_solipsism

--- lib/tfp_metaprogramming/meta_class/macros/lone_wolf.rb
+++ lib/tfp_metaprogramming/meta_class/macros/lone_wolf.rb
@@ -2,6 +2,11 @@ module TfpMetaprogramming
   module MetaClass
     module Macros
       class LoneWolf
+        class << self
+          def resolve_solipsism
+            'My loneliness is killing me. I must confess I still believe!'
+          end
+        end
         attr_accessor :name, :sex, :alpha
         def initialize(name: 'Kujo', sex: 'male', alpha: true)
           @name, @sex, @alpha = name, sex, alpha
--- spec/meta_class/macros/lone_wolf_spec.rb
+++ spec/meta_class/macros/lone_wolf_spec.rb
@@ -1,6 +1,5 @@
 describe TfpMetaprogramming::MetaClass::Macros::LoneWolf do
   let(:myWolf) { TfpMetaprogramming::MetaClass::Macros::LoneWolf.new }
-
   describe 'instance_variables' do
     it 'has a @name' do
       expect(myWolf.instance_variables).to include(:@name)
@@ -13,15 +12,29 @@ describe TfpMetaprogramming::MetaClass::Macros::LoneWolf do
     end
   end
   describe 'methods' do
-    describe '#howl' do
-      it 'returns a string that ends with "howling"' do
-        expect(myWolf.howl).to end_with('howling')
+    describe 'instance_methods' do
+      describe '#howl' do
+        it 'returns a string that ends with "howling"' do
+          expect(myWolf.howl).to end_with('howling')
+        end
+      end
+      describe '#hunt' do
+        it 'returns a string that ends with "hunt"' do
+          expect(myWolf.hunt).to end_with('hunt')
+        end
       end
     end
-    describe '#hunt' do
-      it 'returns a string that ends with "hunt"' do
-        expect(myWolf.hunt).to end_with('hunt')
+    describe 'singleton methods' do
+      describe '.resolve_solipsism' do
+        it 'returns a string that ends with "still believe!"' do
+          expect(myWolf.class.resolve_solipsism).to end_with('still believe!')
+        end
+        it 'appears in the LoneWolf.singleton_methods' do
+          expect(myWolf.class.singleton_methods).to include(:resolve_solipsism)
+        end
       end
+
     end
+
   end
 end
