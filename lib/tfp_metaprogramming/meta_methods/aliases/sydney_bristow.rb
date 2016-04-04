2bf02123aa2efdd9eb8fc6918696b45221530f23
theory friction practice <jackson.brebnor@gmail.com>
2016-04-04 19:35:58 -0400

    inline doc/formatting SydneyBristow. also uses :alias_method

--- lib/tfp_metaprogramming/meta_methods/aliases/sydney_bristow.rb
+++ lib/tfp_metaprogramming/meta_methods/aliases/sydney_bristow.rb
@@ -5,22 +5,40 @@ require_relative 'prepended_wig'
 module TfpMetaprogramming
   module MetaMethods
     module Aliases
+      # A class exercising espionage and method_aliases
       class SydneyBristow
         include AroundAlias
+        # due to the nature of refinements only calls
+        # made on the implicit receiver will
+        # make the proper calls to #wig
         using SpyRefinement
+
+        # prepends the PrependedWig module to
+        # the cref, overriding methods defined
+        # in later modules (including the clas itself)
         def self.prepend_wig
           prepend PrependedWig
         end
 
         def initialize(occupation: 'spy', hobby: 'throwing fierce lqqks')
-          @occupation, @hobby = occupation, hobby
+          @occupation = occupation
+          @hobby = hobby
         end
 
+        # throws fierce lqqks ideal for international malfeasance
+        # @return [String]
         def throw_fierce_lqqks
           'Here I am, giving you Soviet-Satellite realness'
         end
-        alias :fierce_looks_by_proxy :throw_fierce_lqqks
 
+        # aliases #throw_firece_lqqks, preseving
+        # original functionality in case of override
+        alias_method :fierce_looks_by_proxy, :throw_fierce_lqqks
+
+        # an override of AroundAlias#around_espionage
+        # which appeneds the orginal, unaltered #around_espionage
+        # method via its alias AroundAlias#convolutions
+        # @return [String]
         def around_espionage
           'this method is overriding #around_alias
           defined in the module, but will concateate
@@ -28,14 +46,22 @@ module TfpMetaprogramming
           #convolutions alias defined in the included module.
           here it comes' << convolutions
         end
+
+        # A wig method to be overwritten
+        # by Aliases::SpyRefinement
+        # and Aliases::PrependedWig
+        # @return [String] 'I have a regular wig'
         def wig
           'I have regular wig.'
-          # wig
         end
+
+        # because SydneyBristow is currently the only scope
+        # calling 'using Spyrefinement' the only way
+        # to call SpyRefinement#wig is to use the implicit receiver
+        # @return [String] a refined version of #wig
         def call_wig
           wig
         end
-        # alias :old_wig :wig
       end
     end
   end
