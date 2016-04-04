2403a21e6c166e70cda1829c63580e3472edc9aa
theory friction practice <jackson.brebnor@gmail.com>
2016-04-04 19:41:09 -0400

    docs/formatting for Aliases::SpyRefinement:

--- lib/tfp_metaprogramming/meta_methods/aliases/spy_refinement.rb
+++ lib/tfp_metaprogramming/meta_methods/aliases/spy_refinement.rb
@@ -2,8 +2,14 @@ module TfpMetaprogramming
   module MetaMethods
     module Aliases
       class SydneyBristow;end
+      # A refinment module applied to
+      # Alias::SydneyBristow
       module SpyRefinement
         refine SydneyBristow do
+          # a wrapper into SydneyBristow#wig
+          # @note only available within scope of
+          #   'using Spyrefinement'
+          # @return [String] '...this is my refined wig'
           def wig
             super << 'this is my refined wig'
           end
