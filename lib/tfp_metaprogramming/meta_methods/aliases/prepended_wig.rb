4d4e34da15a224269203b39fd4dc57bfc73dcc8f
theory friction practice <jackson.brebnor@gmail.com>
2016-04-04 19:04:26 -0400

    adds inline documentation/formatting to Aliases::PrependedWig

--- lib/tfp_metaprogramming/meta_methods/aliases/prepended_wig.rb
+++ lib/tfp_metaprogramming/meta_methods/aliases/prepended_wig.rb
@@ -1,7 +1,15 @@
 module TfpMetaprogramming
   module MetaMethods
     module Aliases
+      # This module will insert itself before
+      # SydneyBristow in the cref
       module PrependedWig
+        # as a result of its position in the cref
+        # during method lookup, this method will
+        # be called before all other wigs.
+        # it also calls super to demonstrate
+        # its capacity as a hook/wrapper
+        # @return [String]
         def wig
           'a cleaner way to wrap my wig' << super
         end
