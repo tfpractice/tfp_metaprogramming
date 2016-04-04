253a817c31761466941b5779b3843c05fc743a3f
theory friction practice <jackson.brebnor@gmail.com>
2016-04-04 13:46:11 -0400

    cleans up spec syntax

--- spec/object_model/monkey_patch_spec.rb
+++ spec/object_model/monkey_patch_spec.rb
@@ -1,47 +1,23 @@
 describe 'monkey_patch/String' do
   let(:myString) { "I am a string to be monkeyed" }
-
-  # describe '.ancestry' do
-    # it 'returns the ancestor chain' do
-      # expect(myString.class.ancestors).to include(Comparable)
-      # expect(myString.class).to respond_to(:ancestry)
-
-    # end
-
-
-  # end
-  # describe '.nesting' do
-  	# it 'returns the Module#nesting attribute' do
-  		# puts myString.class.nesting
-  		# expect(String).to respond_to(:nesting)
-  	# end
-    # 
-  # end
   describe '#make_ape' do
-  	it 'adds #make_ape method to core class' do
-  		expect(myString).to respond_to(:make_ape)
-  		
-  	end
+    it 'adds #make_ape method to core class' do
+      expect(myString).to respond_to(:make_ape)
+    end
     it 'dynamically assigns an @ape instance variable on the string' do
-    	myString.make_ape("curious george")
-    	puts myString.instance_variables
-    	expect(myString.instance_variables).to include(:@ape)
-    	# expect(myString.ape).to eq("curious george")
+      myString.make_ape("curious george")
+      expect(myString.instance_variables).to include(:@ape)
     end
   end
   describe '#monkey' do
-
     it 'adds #monkey to the Ruby Core String class' do
-    	# puts myString.class.constants
       expect(myString.class).to eq(String)
     end
     it 'responds to #monkey' do
       expect(myString).to respond_to(:monkey)
-
     end
     it 'retuns "I love bananas, and have opposable thumbs" ' do
       expect(myString.monkey).to eq("I love bananas, and have opposable thumbs")
     end
   end
-
 end
--- spec/object_model/refinement/r_string_spec.rb
+++ spec/object_model/refinement/r_string_spec.rb
@@ -2,18 +2,13 @@ describe TfpMetaprogramming::ObjectModel::RString do
   let(:rString) { TfpMetaprogramming::ObjectModel::RString.new("this is a refined string") }
   describe '.ancestors' do
     it 'includes String' do
-      puts rString.class.ancestors
       expect(rString.class.ancestors).to include(String)
     end
   end
   describe '#refined_method' do
     it 'will not respond_to? :refined_method due to method lookup issues with refinements' do
       expect(rString).not_to respond_to(:refined_method)
-
     end
-
-
-
   end
   describe '#call_refinement' do
     it 'calls refined_method while still in scope via Module#using' do
@@ -21,7 +16,6 @@ describe TfpMetaprogramming::ObjectModel::RString do
     end
     it 'returns "The String class has been refined with..."' do
       expect(rString.call_refinement).to start_with("The String class has been refined with")
-      # expect(rString.call_refinement).to eq("The String class has been refined with this aptly named refined method, and is limited to the scope of the including modules, thus avoiding #monkeyPatchingProblems")
     end
   end
 end
