b680370f39978b6d34ee9b2b8d4fabe1e28ebc41
theory friction practice <jackson.brebnor@gmail.com>
2016-04-04 19:52:27 -0400

    docs/formatting MetaClass::SuperProxy

--- lib/tfp_metaprogramming/meta_class/super_proxy.rb
+++ lib/tfp_metaprogramming/meta_class/super_proxy.rb
@@ -1,15 +1,21 @@
 module TfpMetaprogramming
   module MetaClass
+    # a class that acts specifically as
+    # a parameter to inline class definition
+    # in MetaClass#define_inline_class
     class SuperProxy
       attr_accessor :num
       def initialize(num: 30)
         @num = num
       end
 
+      # a description of this class and
+      # its role within the MetaClass module
+      # @return [String]
       def description
         'I am the SuperProxy class and I exist
-      	only as  a superclass for the yet to be defined
-      	MetaClass::InlineClass'
+        only as  a superclass for the yet to be defined
+        MetaClass::InlineClass'
       end
     end
   end
