953d2b18371bb3b17a7799e01746da29024d8dd1
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 09:33:58 -0400

    docs/formats ...::Macros::PackWolf < LoneWolf

--- lib/tfp_metaprogramming/meta_class/macros/pack_wolf.rb
+++ lib/tfp_metaprogramming/meta_class/macros/pack_wolf.rb
@@ -2,8 +2,14 @@ require_relative 'lone_wolf'
 module TfpMetaprogramming
   module MetaClass
     module Macros
+      # a subclass of LoneWolf that
+      # deprecates some methods
       class PackWolf < LoneWolf
-
+        # a class method that deprecates
+        # an old_method, outputs a warning
+        # to stderr, and calls the new_method
+        # @param [Symbol, String] old_method
+        # @param [Symbol, String] new_method
         def self.deprecate(old_method, new_method)
           define_method(old_method) do |*args, &block|
             warn "Warning: #{old_method}() has been deprecated, use #{new_method} instead"
@@ -11,19 +17,32 @@ module TfpMetaprogramming
           end
         end
 
+        # an updated version of the
+        # deprecated LoneWolf#howl and
+        # has the identical functionality
         def new_howl
           'I am a lone wolf howling'
         end
+
+        # an updated version of the
+        # deprecated LoneWolf#hunt and
+        # has the identical functionality
         def new_hunt
           'I am a lone wolf on the hunt'
         end
+
+        # an instance method particular
+        # to this subclass
+        # @return [String]
         def pack_howl
           'I am a wolf howling with the pack'
         end
 
+        # see(#deprecate)
         deprecate :howl, :new_howl
-        deprecate :hunt, :new_hunt
 
+        # see(#deprecate)
+        deprecate :hunt, :new_hunt
       end
     end
   end
