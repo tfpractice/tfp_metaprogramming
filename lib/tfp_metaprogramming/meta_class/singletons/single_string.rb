ecf65c0ec19f19dc8a5ed877bf9946d9fbb92d0d
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 10:23:09 -0400

    docs/formats ....::Singletons::SingleString

--- lib/tfp_metaprogramming/meta_class/singletons/single_string.rb
+++ lib/tfp_metaprogramming/meta_class/singletons/single_string.rb
@@ -2,17 +2,23 @@ require_relative 'singlizer'
 module TfpMetaprogramming
   module MetaClass
     module Singletons
+      # a class inheriting from Object::String
+      # focusing specifically on singleton methods
       class SingleString < String
-
+        # adds the #lifestyle method to
+        # the singleton class of a specific
+        # instance of SingleString
         def hermitize
           class << self
+            # describes the even deeper
+            # singleness of a particular object
+            # @return [String]
             def lifestyle
               'I live alone in the mountains.
-        			 I write obscure mathematical texts.
-        			 I am a hermit'
+               I write obscure mathematical texts.
+               I am a hermit'
             end
           end
-
         end
       end
     end
