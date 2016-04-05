a9f0583b374608f16e257a42cfed1be82a1df41c
theory friction practice <jackson.brebnor@gmail.com>
2016-04-04 21:07:40 -0400

    docs/formats Extensions::ViaInclude

--- lib/tfp_metaprogramming/meta_class/extensions/via_include.rb
+++ lib/tfp_metaprogramming/meta_class/extensions/via_include.rb
@@ -1,10 +1,15 @@
 module TfpMetaprogramming
   module MetaClass
     module Extensions
+      # adds singleton_methods to
+      # objects (via Object#include)
       module ViaInclude
+        # applies an instance method to
+        # the singleton class of Extensions::Extender
+        # therby functioning as a class method
         def included_c_method
           'I am the class method defined in a module,
-          	 and applied to the receiver via include'
+             and applied to the receiver via include'
         end
       end
     end
