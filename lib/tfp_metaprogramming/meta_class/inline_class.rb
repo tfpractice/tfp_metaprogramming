a3cb307093ae9b1b4d888eb6d42e676d31039e9a
theory friction practice <jackson.brebnor@gmail.com>
2016-04-04 20:01:42 -0400

    docs/formats MetaClass.define_inline_method

--- lib/tfp_metaprogramming/meta_class/inline_class.rb
+++ lib/tfp_metaprogramming/meta_class/inline_class.rb
@@ -1,8 +1,17 @@
 require_relative 'super_proxy'
 module TfpMetaprogramming
   module MetaClass
+    # using the  scope_flattened
+    # define_singleton_method,
+    # MetaClass#create_inline_class defines
+    # a subclass of MetaClass::SuperProxy
+    # and then names it InlineClass
+    # @return [Class] MetaClass::InlineClass
     self.define_singleton_method :create_inline_class do
       c = Class.new(SuperProxy) do
+        # describes the specificity
+        # of this to this sublcass
+        # @return [String]
         def inline_method
           'I am the inline_method called from the dynamically
           defined method MetaClass#create_inline_class'
