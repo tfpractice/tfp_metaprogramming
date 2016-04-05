30f53143a0a0c0f79327e3d1f40e6bb35e176226
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 09:11:55 -0400

    docs/formats MetaClass::ClassModifier.add_new_method_to_class

--- lib/tfp_metaprogramming/meta_class/class_modifier.rb
+++ lib/tfp_metaprogramming/meta_class/class_modifier.rb
@@ -2,9 +2,17 @@ require_relative 'mod_class'
 
 module TfpMetaprogramming
   module MetaClass
+    # a namespace that modifies
+    # the sister class MetaClass::ModClass
     module ClassModifier
+      # adds a new method to the receiving
+      # class via class_eval
+      # @param [Class] rec_class
       def self.add_new_method_to_class(rec_class)
         rec_class.class_eval do
+          # reports that a new instance
+          # method has been defined
+          # @return[String]
           def report_new_method
             'This method comes from the ClassModifier module'
           end
