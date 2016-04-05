2bd5314b24c907a39bf0e9c0d76050ceee5bffd9
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 10:28:39 -0400

    docs/formats ...::Singletons::Singlizer

--- lib/tfp_metaprogramming/meta_class/singletons/singlizer.rb
+++ lib/tfp_metaprogramming/meta_class/singletons/singlizer.rb
@@ -1,19 +1,29 @@
 module TfpMetaprogramming
   module MetaClass
     module Singletons
+      # a module that defines methods
+      # via Object#instance_eval rather
+      # than through module inclusion
       module Singlizer
-        def self.eval_monog_onto_single_string(iString)
-          iString.instance_eval do
+        # defines SingleString#monogamize
+        # via instance_eval
+        # @param [SingleString, String] i_string
+        def self.eval_monog_onto_single_string(i_string)
+          i_string.instance_eval do
+            # appends "I am monogamous" to i_string
             def self.monogamize
               self << ' I am monogamous'
             end
           end
         end
 
-        def self.nest_mingle_method_onto_single_string(iString)
-          def iString.mingle
+        # defines SingleString#mingle
+        # via nested method definition
+        # @param [SingleString, String] i_string
+        def self.nest_mingle_method_onto_single_string(i_string)
+          def i_string.mingle
+            # appends " and I am ready to mingle" to i_string
             self << ' and I am ready to mingle'
-
           end
         end
       end
