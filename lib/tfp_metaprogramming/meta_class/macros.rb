fb821e7c34cbf1557f359dd08de813a21fbf44cc
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 09:21:22 -0400

    docs/formats MetaMethods::Macros.force_wolf_to_contemplate_solipsism

--- lib/tfp_metaprogramming/meta_class/macros.rb
+++ lib/tfp_metaprogramming/meta_class/macros.rb
@@ -1,12 +1,22 @@
 module TfpMetaprogramming
   module MetaClass
+    # A module exploring class macros
     module Macros
-      class LoneWolf;end
+      # a class to be modified by Macros
+      # module, prematurely defined to
+      # avoid Constant Lookup issues
+      class LoneWolf; end
+
+      # a clas method that adds the
+      # LoneWolf.contemplate_solipsism
+      # singleton_method
       def self.force_wolf_to_contemplate_solipsism
+        # opens the LoneWolf singleton class
         class << LoneWolf
+          # @return [String]
           def contemplate_solipsism
             "I am a lone wolf contemplating the bleakness
-    				of my reality. This is all due to a singleton method"
+            of my reality. This is all due to a singleton method"
           end
         end
       end
