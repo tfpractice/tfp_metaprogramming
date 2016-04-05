b64abe4a72934f582dbf9720e7acf74406cc57f3
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 10:19:23 -0400

    docs/formats ...::MetaClass::Singletons

--- lib/tfp_metaprogramming/meta_class/singletons.rb
+++ lib/tfp_metaprogramming/meta_class/singletons.rb
@@ -1,7 +1,12 @@
 require_relative './singletons/single_string'
 module TfpMetaprogramming
   module MetaClass
+    # A namespace for exploring the
+    # singleton/eigen class and various
+    # singleton method definitions
     module Singletons
+      # using  def Obj.method, adds
+      # #my_singleton to Singletons::SingleSting
       def self.add_my_singleton_to_single_string
         def SingleString.my_singleton
           'I am the my_singleton method defined in the singletons module'
