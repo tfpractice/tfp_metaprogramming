1eba8e60d33bee5f62e9132d529cb77202346625
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 09:03:53 -0400

    docs.formats Extensions::Extender

--- lib/tfp_metaprogramming/meta_class/extensions/extender.rb
+++ lib/tfp_metaprogramming/meta_class/extensions/extender.rb
@@ -3,16 +3,28 @@ require_relative 'via_extend'
 module TfpMetaprogramming
   module MetaClass
     module Extensions
+      # receives various methods from
+      # ViaInclude and ViaExtendModules
       class Extender
+        # opens eigenclass to include
+        # ViaInclude instance methods
+        # as class methods
         class << self
           include ViaInclude
         end
+        # using Object#extend, instance
+        # methods from ViaExtends are added
+        # as class methods
         extend ViaExtend
         attr_accessor :num
         def initialize(num: 40)
           @num = num
         end
 
+        # applies Object#eextend
+        # to an instance of this class
+        # allowing singleton_methods on
+        # specific instances
         def extend_instance
           self.extend ViaExtend
         end
