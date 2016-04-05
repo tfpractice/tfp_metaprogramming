c081bd00f5e77d94f9522dc1d8e2a8160afae9c4
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 10:15:15 -0400

    docs/formats ...::MetaClass::ModClass

--- lib/tfp_metaprogramming/meta_class/mod_class.rb
+++ lib/tfp_metaprogramming/meta_class/mod_class.rb
@@ -1,14 +1,18 @@
 module TfpMetaprogramming
   module MetaClass
+    # A class to  explore class_variables,
+    # class_instance_variables, and to be
+    # modified by sister module ClassModifier.
     class ModClass
       attr_reader :num
       def initialize(num: 5)
         @num = num
       end
+      # @!scope class
       @class_ivar = 25
+
+      # @!scope class
       @@c_var = 20
-
-
     end
   end
 end
