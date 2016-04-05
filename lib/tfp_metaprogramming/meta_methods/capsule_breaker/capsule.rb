d9a69f46ff0202a02a2e5f65292f44e0c8597ddc
theory friction practice <jackson.brebnor@gmail.com>
2016-04-05 10:34:35 -0400

    docs/formats ...::MetaMethods::Capsule

--- lib/tfp_metaprogramming/meta_methods/capsule_breaker/capsule.rb
+++ lib/tfp_metaprogramming/meta_methods/capsule_breaker/capsule.rb
@@ -1,27 +1,31 @@
-# upon initialization, this class sets
-# a mass attribute which can be publicly
-# incremented or decremented
-# but only reset privately
+
 module TfpMetaprogramming
   module MetaMethods
     module CapsuleBreaker
-
+      # upon initialization, this class sets
+      # a mass attribute which can be publicly
+      # incremented or decremented
+      # but only reset privately
       class Capsule
         attr_reader :mass
         def initialize(mass = 0)
           @mass = mass
         end
 
+        # increments mass by 1
         def increment
           self.mass += 1
         end
 
+        # decrements mass by 1
         def decrement
           self.mass -= 1
         end
 
         private
 
+        # resets the mass to val param
+        # @param [Numeric] val
         def mass=(val)
           @mass = val
         end
